22413ee9095ce3d4b2557a2d930869e777339b8a
El_Am1go <saguilar.arana10@gmail.com>
2026-04-16 13:39:41 -0600

    Comments

--- Vector3D.java
+++ Vector3D.java
@@ -1,5 +1,6 @@
 public class Vector3D {
 
+    // Attributes
     private double x, y, z;
 
     //Constructor
@@ -19,7 +20,7 @@ public class Vector3D {
     public void setY(double y) { this.y = y;}
     public void setZ(double z) { this.z = z;}
 
-
+    // Operations
     public Vector3D add(Vector3D other) {
         return new Vector3D(this.x + other.x, this.y + other.y, this.z + other.z);
     }
@@ -43,7 +44,7 @@ public class Vector3D {
         return new Vector3D(crossX, crossY, crossZ);
     }
 
-    // Normalize & Magnitude  
+    // Magnitude + Normalization
     public double magnitude() {
         return Math.sqrt(x * x + y * y + z * z);
     }
