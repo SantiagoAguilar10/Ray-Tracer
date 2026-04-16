92387105fb97132bef30a30d67b837370ffdde9b
El_Am1go <saguilar.arana10@gmail.com>
2026-04-16 14:15:30 -0600

    Intersection Get & Set

--- Intersection.java
+++ Intersection.java
@@ -1,8 +1,29 @@
 public class Intersection {
-    /**
-     * Distance (t)
-     * Point (vector3D)
-     * Normal (vector3D)
-     * Object (Object3D)
-     */
+
+    // Attributes
+    private double t;
+    private Vector3D point;
+    private Vector3D normal;
+    private Object3D object;
+
+    // Constructor
+    public Intersection(double t, Vector3D point, Vector3D normal, Object3D object) {
+        this.t = t;
+        this.point = point;
+        this.normal = normal;
+        this.object = object;
+    }
+
+    // Getters
+    public double getT() { return t;}
+    public Vector3D getPoint() { return point;}
+    public Vector3D getNormal() { return normal;}
+    public Object3D getObject() { return object;}
+    
+    // Setters
+    public void setT(double t) { this.t = t;}
+    public void setPoint(Vector3D point) { this.point = point;}
+    public void setNormal(Vector3D normal) { this.normal = normal;}
+    public void setObject(Object3D object) { this.object = object;}
+
 }
